ab92761ea05dc66d319ff7aca8338b6e1ca97b5e
Alexander Roehnisch <aroehnisch@info-tech.biz>
2021-02-02 06:31:22 +0100

    Added MapBox MapView and NavigationView. Only MapView works.

--- android/app/src/main/kotlin/com/example/flutter_platformview_example/FirstWidget.kt
+++ android/app/src/main/kotlin/com/example/flutter_platformview_example/FirstWidget.kt
@@ -3,6 +3,10 @@ package com.example.flutter_platformview_example
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.navigation.ui.NavigationView
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -13,13 +17,24 @@ import io.flutter.plugin.platform.PlatformView
 class FirstWidget internal constructor(context: Context, id: Int, messenger: BinaryMessenger) : PlatformView, MethodCallHandler {
     private val view: View
     private val methodChannel: MethodChannel
+    val mapView: MapView
 
     override fun getView(): View {
         return view
     }
 
     init {
+        Mapbox.getInstance(context, context.getString(R.string.mapbox_access_token))
         view = LayoutInflater.from(context).inflate(R.layout.first_widget, null)
+
+        mapView = view.findViewById(R.id.mapView)
+        mapView?.onCreate(null)
+        mapView?.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+            }
+        }
+
         methodChannel = MethodChannel(messenger, "plugins/first_widget_$id")
         methodChannel.setMethodCallHandler(this)
     }
--- android/app/src/main/kotlin/com/example/flutter_platformview_example/SecondWidget.kt
+++ android/app/src/main/kotlin/com/example/flutter_platformview_example/SecondWidget.kt
@@ -3,6 +3,9 @@ package com.example.flutter_platformview_example
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.navigation.ui.NavigationView
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -13,13 +16,19 @@ import io.flutter.plugin.platform.PlatformView
 class SecondWidget internal constructor(context: Context, id: Int, messenger: BinaryMessenger) : PlatformView, MethodCallHandler {
     private val view: View
     private val methodChannel: MethodChannel
+    val navView: NavigationView
 
     override fun getView(): View {
         return view
     }
 
     init {
+        Mapbox.getInstance(context, context.getString(R.string.mapbox_access_token))
         view = LayoutInflater.from(context).inflate(R.layout.second_widget, null)
+
+        navView = view.findViewById(R.id.navigationView)
+        navView?.onCreate(null)
+
         methodChannel = MethodChannel(messenger, "plugins/second_widget_$id")
         methodChannel.setMethodCallHandler(this)
     }
